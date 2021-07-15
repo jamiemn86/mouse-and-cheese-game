458873cae9de77839fac1e9ecabe700267cc08a5
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-07-15 19:55:46 +0200

    Updates to startCountdown()

--- js/countdown.js
+++ js/countdown.js
@@ -1,12 +1,12 @@
 let counter = 45;
-let firstCounterStopped;
+let firstCounterStopped = 45;
 let counterStopped = [];
 
 function startCountdown() {
   setInterval(function () {
+    let span = document.getElementById('count');
     counter--;
     if (counter >= 0) {
-      span = document.getElementById('count');
       span.innerHTML = `Countdown timer: ${counter} seconds`;
     }
     if (counter === 0) {
@@ -16,6 +16,7 @@ function startCountdown() {
     if (mouseCoordinates.mouseX > 380 && mouseCoordinates.mouseY < 85) {
       counterStopped.push(counter);
       firstCounterStopped = counterStopped[0];
+      span.innerHTML = `Countdown timer: ${firstCounterStopped} seconds`;
     }
   }, 1000);
 }
