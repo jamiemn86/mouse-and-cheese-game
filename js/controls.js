033f07c4d0da722d7f26533d1a1d026add48ac83
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-07-15 19:09:24 +0200

    Adding keepMouseInBoundary()

--- js/controls.js
+++ js/controls.js
@@ -1,21 +1,38 @@
 function moveUp() {
   mouse.y -= 15;
   mouseCoordinates.mouseY = mouse.y;
+  keepMouseInBoundary();
 }
 
 function moveDown() {
   mouse.y += 15;
   mouseCoordinates.mouseY = mouse.y;
+  keepMouseInBoundary();
 }
 
 function moveRight() {
   mouse.x += 15;
   mouseCoordinates.mouseX = mouse.x;
+  keepMouseInBoundary();
 }
 
 function moveLeft() {
   mouse.x -= 15;
   mouseCoordinates.mouseX = mouse.x;
+  keepMouseInBoundary();
+}
+
+function keepMouseInBoundary() {
+  if (mouse.x >= 465) {
+    mouse.x = 465;
+  } else if (mouse.x <= 5) {
+    mouse.x = 5;
+  }
+  if (mouse.y >= 455) {
+    mouse.y = 455;
+  } else if (mouse.y <= 5) {
+    mouse.y = 5;
+  }
 }
 
 function enableControls() {
